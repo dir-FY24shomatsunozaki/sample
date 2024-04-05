eb2abe633a052545af332b25cf91c2ccd40e377a
dir-FY24shomatsunozaki <shoma.tsunozaki.t825@dirbato.co.jp>
2024-04-05 16:31:56 +0900

    Lesson2.7.2

---
+++ sample/src/sample/WhileSample.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class WhileSample {
+
+    public static void main(String[] args) {
+        
+        var i = 2;
+        
+        while( i <= 1024) {
+            System.out.println(i);
+            i = i*2;
+        }
+    }
+
+}
