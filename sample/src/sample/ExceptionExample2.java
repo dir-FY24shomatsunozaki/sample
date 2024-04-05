e96647d6b04eda6a9d48807329912a1fc8187541
dir-FY24shomatsunozaki <shoma.tsunozaki.t825@dirbato.co.jp>
2024-04-05 17:29:31 +0900

    practice

---
+++ sample/src/sample/ExceptionExample2.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class ExceptionExample2 {
+
+    public static void main(String[] args) {
+        // TODO 自動生成されたメソッド・スタブ
+       // sssssssssssss
+        
+
+    }
+
+}
